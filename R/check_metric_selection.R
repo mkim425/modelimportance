f09b10c7f9073d3a101cfe2b17d1fb00a0e0c0e0
mkim425 <minsu@umass.edu>
2024-10-31 11:10:38 -0400

    do not export `check_metric_selection()` and realign output types and score metrics

--- R/check_metric_selection.R
+++ R/check_metric_selection.R
@@ -1,33 +1,35 @@
 #' Validate metric selection for output types
 #'
 #' @param output_type A character string specifying the type of prediction
-#' output. Should be one of
-#' "mean", "median", "quantile", "pmf", "cdf", or "sample".
+#' output. Should be one of "mean", "median", "quantile", "pmf", or "sample".
 #' @param scoring_rule A character string specifying the scoring rule to use
 #' as input
 #'
-#' @return TRUE if inputs are valid, otherwise an error is raised.
-#' @export
+#' @return An error is raised if any of the inputs is not valid.
 #'
 #' @examples \dontrun{
 #' # Invalid combinations
-#' check_metric_selection("mean", "WIS") # Raises an error
-#' check_metric_selection("quantile", "MAE") # Raises an error
-#' check_metric_selection("mean", "MSE") # Returns TRUE
-#' check_metric_selection("quantile", "WIS") # Returns TRUE
+#' check_metric_selection("mean", "wis") # Raises an error
+#' check_metric_selection("quantile", "ae_point") # Raises an error
+#' check_metric_selection("mean", "se_point") # Returns TRUE
+#' check_metric_selection("quantile", "wis") # Returns TRUE
 #' }
 check_metric_selection <- function(output_type, scoring_rule) {
-  if (output_type %in% c("mean", "median")) {
-    if (!(scoring_rule %in% c("MAE", "MSE"))) {
-      stop("The scoring rule needs to be either MAE or MSE")
+  if (output_type == "median") {
+    if (scoring_rule != "ae_point") {
+      stop("The scoring rule needs to be ae_point")
+    }
+  } else if (output_type == "mean") {
+    if (scoring_rule != "se_point") {
+      stop("The scoring rule needs to be se_point")
     }
   } else if (output_type == "quantile") {
-    if (!(scoring_rule %in% c("WIS", "Logscore"))) {
-      stop("The scoring rule needs to be either WIS or Logscore")
+    if (scoring_rule != "wis") {
+      stop("The scoring rule needs to be wis")
     }
-  } else if (output_type %in% c("pmf", "cdf")) {
-    if (scoring_rule != "CRPS") {
-      stop("The scoring rule needs to be CRPS")
+  } else if (output_type == "pmf") {
+    if (scoring_rule != "logscore") {
+      stop("The scoring rule needs to be logscore")
     }
   } else if (output_type == "sample") {
     stop("sample model output type is under development and not yet supported.
@@ -35,6 +37,4 @@ check_metric_selection <- function(output_type, scoring_rule) {
   } else {
     stop("invalid output type.")
   }
-  # return TRUE if all validations pass
-  return(TRUE)
 }
