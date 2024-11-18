f79b50e9b706365f37bb0fe3140e2868b1f80d73
mkim425 <minsu@umass.edu>
2024-11-18 14:12:20 -0500

    use a short name when checking

--- R/validate_inputs.R
+++ R/validate_inputs.R
@@ -43,9 +43,10 @@ validate_inputs <- function(forecast_data, target_data,
                             importance_algorithm = c("lomo", "lasomo"),
                             subset_wt = c("equal", "perm_based"),
                             scoring_rule = c(
-                                    "ae_point", "se_point", "wis", "logscore"
+                              "ae_point", "se_point", "wis", "logscore"
                             ),
                             na_action = c("worst", "average", "drop")) {
+  window_len <- training_window_length
   # validate inputs
   if (!is.data.frame(forecast_data)) {
     stop("Invalid input: 'forecast_data' must be a data frame.")
@@ -57,8 +58,7 @@ validate_inputs <- function(forecast_data, target_data,
     stop("Invalid value for 'weighted'. It must be either TRUE or FALSE.
          Default is FALSE.")
   }
-  if (!(is.numeric(training_window_length) &&
-        training_window_length == as.integer(training_window_length))) {
+  if (!(is.numeric(window_len) && window_len == as.integer(window_len))) {
     stop("Invalid value for 'training_window_length'. It must be an integer.
          Default is 0.")
   }
