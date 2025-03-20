f7ec0ef347ebe4f090e30d9e4bc56e4e4875fa3d
mkim425 <minsu@umass.edu>
2025-03-20 16:45:06 -0400

    add detailed comment

--- tests/testthat/test-score_untrained.R
+++ tests/testthat/test-score_untrained.R
@@ -36,16 +36,24 @@ test_that("Compute when using 'linear pool' with 'mean' output type", {
       output_type == unique(dat$output_type) &
       location == unique(dat$location)
   )
+  # replace the target oracle value with a simple value
   target_data$oracle_value[idx] <- 10
-  # calculate the expected output
+  # calculate the squared error of the ensembles
   se_ensemble <- c(
+    # squared error of the ensemble built with all models
     (mean(dat$value) - target_data$oracle_value[idx])^2,
+    # squared error of the ensemble built without the first model
     (mean(dat$value[2:3]) - target_data$oracle_value[idx])^2,
+    # squared error of the ensemble built without the second model
     (mean(dat$value[c(1, 3)]) - target_data$oracle_value[idx])^2,
+    # squared error of the ensemble built without the third model
     (mean(dat$value[1:2]) - target_data$oracle_value[idx])^2
   )
+  # calculate importance scores by subtracting the error of the ensemble-all
+  # from each of ensembles built without one model
   expected_importance <- (se_ensemble - se_ensemble[1])[-1]
 
+  # test: compare the calculated importance with the expected importance
   expect_equal(
     score_untrained(
       single_task_data = dat,
@@ -66,22 +74,30 @@ test_that("Assign NAs for missing data", {
   # replace with simple values for easy calculation
   dat <- mean_data_list[[1]]
   dat$value <- c(30, 18)
+  # find index of target_data corresponding to dat
   idx <- with(
     target_data,
     target_end_date == unique(dat$target_end_date) &
       output_type == unique(dat$output_type) &
       location == unique(dat$location)
   )
+  # replace the target oracle value with a simple value
   target_data$oracle_value[idx] <- 10
-  # calculate the expected output
+  # calculate the squared error of the ensembles
   se_ensemble <- c(
+    # squared error of the ensemble built with all models
     (mean(dat$value) - target_data$oracle_value[idx])^2,
+    # squared error of the ensemble built without the first model
     (mean(dat$value[2]) - target_data$oracle_value[idx])^2,
+    # squared error of the ensemble built without the second model
     (mean(dat$value[1]) - target_data$oracle_value[idx])^2,
+    # assign NA for missing data
     NA
   )
+  # calculate importance scores by subtracting the error of the ensemble-all
+  # from each of ensembles built without one model
   expected_importance <- (se_ensemble - se_ensemble[1])[-1]
-
+  # test: compare the calculated importance with the expected importance
   expect_equal(
     score_untrained(
       single_task_data = dat,
