dd6859f74faa40861410a97695eaa2ac8557e4b4
Minsu Kim <minsu@umass.edu>
2025-05-21 13:26:35 -0400

    Reorganize files in inst/get-testdata/ (#27)
    * reduce the target data size by saving the rows in use
    
    * Revise object names to prevent duplication and enhance clarity, and update the file names to indicate output type, ensemble method, and LOMO
    
    * collects all code to generate forecasting and target datasets used in unit tests by output type

--- inst/get-testdata/exp_imp_pmf_untrained_lomo.R
+++ inst/get-testdata/exp_imp_pmf_untrained_lomo.R
@@ -1,52 +1,50 @@
-## Data for testing the score_untrained example: pmf output
+## Generate expected importance scores for the untrained ensemble models
+## with pmf output in LOMO
+
 # load the package to make its internal functions available
 devtools::load_all()
-## target data
+
+# target data
 target_data_pmf <- readRDS(
-  testthat::test_path("testdata/flu_example_target_data.rds")
-) |> mutate(target_end_date = as.Date(target_end_date))
-
-## forecast data with pmf output
-forecast_pmf <- readRDS(
-  testthat::test_path("testdata/flu_example_pmf_model_output.rds")
-)
-valid_tbl_pmf <- validate_input_data(forecast_pmf, target_data_pmf)
-pmf_data_list <- split_data_by_task(valid_tbl_pmf,
-  weighted = FALSE,
-  training_window_length = 0
+  testthat::test_path("testdata/target_pmf.rds")
 )
 
-# replace with simple values for easy calculation
-dat_pmf <- pmf_data_list[[16]]
-
+# forecast data with pmf output
+dat_pmf <- readRDS(
+  testthat::test_path("testdata/dat_pmf.rds")
+)
 model_id_list <- unique(dat_pmf$model_id)
 
 ## Case 1: no missing data and 'linear pool' ensemble
-# ensemble built from all models
-ens_df <- linear_pool(dat_pmf, model_id = "ens_all")
-# construct ensembles without each model
+# create an ensemble using all models and store it in 'df_ensembles' dataframe.
+df_ensembles <- linear_pool(dat_pmf, model_id = "ens_all")
+# create ensembles without each model and add them in the 'df_ensembles'
 model_names <- unique(dat_pmf$model_id)
 for (i in seq_along(model_names)) {
   sub_data <- dat_pmf |> filter(model_id != model_names[i])
   sub_ens <- linear_pool(sub_data,
     model_id = paste0("ens_wo_", model_names[i])
   )
-  ens_df <- bind_rows(ens_df, sub_ens)
+  df_ensembles <- bind_rows(df_ensembles, sub_ens)
 }
 
 # evaluate each ensemble
-ens_df <- score_model_out(
-  model_out_tbl = ens_df,
+ensemble_scores <- score_model_out(
+  model_out_tbl = df_ensembles,
   oracle_output = target_data_pmf,
   metrics = "log_score"
 ) |>
   # calculate importance scores: subtract the error of the ensemble-all
   mutate(importance = log_score - log_score[1]) |>
   filter(model_id != "ens_all")
-ens_df$model_id <- sub("^ens_wo_", "", ens_df$model_id)
-# expected importance scores with pmf output and linear pool
-exp_imp_pmf1 <- ens_df |>
+
+# get dataframe including model_id and each model's importance score
+model_imp_scores <- ensemble_scores |>
   select(model_id, importance) |>
+  mutate(model_id = sub("^ens_wo_", "", model_id))
+
+# expected importance scores with pmf output and linear pool
+exp_imp_pmf1 <- model_imp_scores |>
   mutate(
     ens_mthd = "linear_pool-NA",
     algorithm = "lomo",
@@ -55,12 +53,12 @@ exp_imp_pmf1 <- ens_df |>
   as.data.frame()
 
 ## Case 2: no missing data and 'simple_ensemble' using agg_fun = mean
-# ensemble built from all models
-ens_df_simple <- simple_ensemble(
+# create an ensemble using all models and store it in 'df_ensembles_simple'
+df_ensembles_simple <- simple_ensemble(
   dat_pmf,
   model_id = "ens_all", agg_fun = "mean"
 )
-# construct ensembles without each model
+# create ensembles without each model and add them in the 'df_ensembles_simple'
 model_names <- unique(dat_pmf$model_id)
 for (i in seq_along(model_names)) {
   sub_data <- dat_pmf |> filter(model_id != model_names[i])
@@ -68,20 +66,24 @@ for (i in seq_along(model_names)) {
     model_id = paste0("ens_wo_", model_names[i]),
     agg_fun = "mean"
   )
-  ens_df_simple <- bind_rows(ens_df_simple, sub_ens)
+  df_ensembles_simple <- bind_rows(df_ensembles_simple, sub_ens)
 }
 # evaluate each ensemble
-ens_df_simple <- score_model_out(ens_df_simple,
+ensemble_scores <- score_model_out(df_ensembles_simple,
   target_data_pmf,
   metrics = "log_score"
 ) |>
   # calculate importance scores: subtract the error of the ensemble-all
   mutate(importance = log_score - log_score[1]) |>
   filter(model_id != "ens_all")
-ens_df_simple$model_id <- sub("^ens_wo_", "", ens_df_simple$model_id)
-# expected importance scores with pmf output and simple mean ensemble
-exp_imp_pmf2 <- ens_df_simple |>
+
+# get dataframe including model_id and each model's importance score
+model_imp_scores <- ensemble_scores |>
   select(model_id, importance) |>
+  mutate(model_id = sub("^ens_wo_", "", model_id))
+
+# expected importance scores with pmf output and simple mean ensemble
+exp_imp_pmf2 <- model_imp_scores |>
   mutate(
     ens_mthd = "simple_ensemble-mean",
     algorithm = "lomo",
@@ -90,12 +92,12 @@ exp_imp_pmf2 <- ens_df_simple |>
   as.data.frame()
 
 ## Case 3: no missing data and 'simple_ensemble' using agg_fun = median
-# ensemble built from all models
-ens_df_simple <- simple_ensemble(
+# create an ensemble using all models and store it in 'df_ensembles_simple'
+df_ensembles_simple <- simple_ensemble(
   dat_pmf,
   model_id = "ens_all", agg_fun = "median"
 )
-# construct ensembles without each model
+# create ensembles without each model and add them in the 'df_ensembles_simple'
 model_names <- unique(dat_pmf$model_id)
 for (i in seq_along(model_names)) {
   sub_data <- dat_pmf |> filter(model_id != model_names[i])
@@ -103,20 +105,24 @@ for (i in seq_along(model_names)) {
     model_id = paste0("ens_wo_", model_names[i]),
     agg_fun = "median"
   )
-  ens_df_simple <- bind_rows(ens_df_simple, sub_ens)
+  df_ensembles_simple <- bind_rows(df_ensembles_simple, sub_ens)
 }
 # evaluate each ensemble
-ens_df_simple <- score_model_out(ens_df_simple,
+ensemble_scores <- score_model_out(df_ensembles_simple,
   target_data_pmf,
   metrics = "log_score"
 ) |>
   # calculate importance scores: subtract the error of the ensemble-all
   mutate(importance = log_score - log_score[1]) |>
   filter(model_id != "ens_all")
-ens_df_simple$model_id <- sub("^ens_wo_", "", ens_df_simple$model_id)
-# expected importance scores with pmf output and simple median ensemble
-exp_imp_pmf3 <- ens_df_simple |>
+
+# get dataframe including model_id and each model's importance score
+model_imp_scores <- ensemble_scores |>
   select(model_id, importance) |>
+  mutate(model_id = sub("^ens_wo_", "", model_id))
+
+# expected importance scores with pmf output and simple median ensemble
+exp_imp_pmf3 <- model_imp_scores |>
   mutate(
     ens_mthd = "simple_ensemble-median",
     algorithm = "lomo",
@@ -127,30 +133,34 @@ exp_imp_pmf3 <- ens_df_simple |>
 ## Case 4: a missing data and 'simple_mean' ensemble
 # data with missing values
 sub_dat_pmf <- dat_pmf |> filter(model_id %in% model_id_list[c(1, 3)])
-# ensemble built from all models
-ens_df <- simple_ensemble(sub_dat_pmf, model_id = "ens_all")
-# construct ensembles without each model
+# create an ensemble using all models and store it in 'df_ensembles'
+df_ensembles <- simple_ensemble(sub_dat_pmf, model_id = "ens_all")
+# create ensembles without each model and add them in the 'df_ensembles'
 model_names <- unique(sub_dat_pmf$model_id)
 for (i in seq_along(model_names)) {
   sub_data <- sub_dat_pmf |> filter(model_id != model_names[i])
   sub_ens <- simple_ensemble(sub_data,
     model_id = paste0("ens_wo_", model_names[i])
   )
-  ens_df <- bind_rows(ens_df, sub_ens)
+  df_ensembles <- bind_rows(df_ensembles, sub_ens)
 }
 # evaluate each ensemble
-ens_df <- score_model_out(ens_df,
+ensemble_scores <- score_model_out(df_ensembles,
   target_data_pmf,
   metrics = "log_score"
 ) |>
   # calculate importance scores: subtract the error of the ensemble-all
   mutate(importance = log_score - log_score[1]) |>
   filter(model_id != "ens_all")
-ens_df$model_id <- sub("^ens_wo_", "", ens_df$model_id)
+
+# get dataframe including model_id and each model's importance score
+model_imp_scores <- ensemble_scores |>
+  select(model_id, importance) |>
+  mutate(model_id = sub("^ens_wo_", "", model_id))
+
 # expected importance scores with pmf output and simple mean ensemble
 exp_imp_pmf4 <- data.frame(model_id = model_id_list) |>
-  left_join(ens_df, by = "model_id") |>
-  select(model_id, importance) |>
+  left_join(model_imp_scores, by = "model_id") |>
   mutate(
     ens_mthd = "simple_mean",
     algorithm = "lomo",
@@ -164,12 +174,6 @@ exp_imp_pmf <- rbind(
 )
 
 # save data
-saveRDS(dat_pmf,
-  file = "tests/testthat/testdata/dat_pmf.rds"
-)
 saveRDS(exp_imp_pmf,
   file = "tests/testthat/testdata/exp_imp_pmf_untrained_lomo.rds"
-)
-saveRDS(target_data_pmf,
-  file = "tests/testthat/testdata/target_pmf.rds"
 )
--- inst/get-testdata/exp_imp_qntl_untrained_lomo.R
+++ inst/get-testdata/exp_imp_qntl_untrained_lomo.R
@@ -1,50 +1,48 @@
-## Data for testing the score_untrained example: quantile output
+## Generate expected importance scores for the untrained ensemble models
+## with quantile output in LOMO
+
 # load the package to make its internal functions available
 devtools::load_all()
-## target data
+
+# target data
 target_data_qntl <- readRDS(
-  testthat::test_path("testdata/flu_example_target_data.rds")
-) |> mutate(target_end_date = as.Date(target_end_date))
-
-## forecast data with quantile output
-forecast_qntl <- readRDS(
-  testthat::test_path("testdata/flu_example_quantile_model_output.rds")
-)
-valid_tbl_qntl <- validate_input_data(forecast_qntl, target_data_qntl)
-qntl_data_list <- split_data_by_task(valid_tbl_qntl,
-  weighted = FALSE,
-  training_window_length = 0
+  testthat::test_path("testdata/target_qntl.rds")
 )
 
-# select a dataset to test
-dat_qntl <- qntl_data_list[[16]]
-# model_id list
+# forecast data with pmf output
+dat_qntl <- readRDS(
+  testthat::test_path("testdata/dat_qntl.rds")
+)
 model_id_list <- unique(dat_qntl$model_id)
 
 ## Case 1: no missing data and 'linear pool' ensemble
-# ensemble built from all models
-ens_df <- linear_pool(dat_qntl, model_id = "ens_all")
-# construct ensembles without each model
+# create an ensemble using all models and store it in 'df_ensembles' dataframe.
+df_ensembles <- linear_pool(dat_qntl, model_id = "ens_all")
+# create ensembles without each model and add them in the 'df_ensembles'
 model_names <- unique(dat_qntl$model_id)
 for (i in seq_along(model_names)) {
   sub_data <- dat_qntl |> filter(model_id != model_names[i])
   sub_ens <- linear_pool(sub_data,
     model_id = paste0("ens_wo_", model_names[i])
   )
-  ens_df <- bind_rows(ens_df, sub_ens)
+  df_ensembles <- bind_rows(df_ensembles, sub_ens)
 }
 # evaluate each ensemble
-ens_df <- score_model_out(ens_df,
+ensemble_scores <- score_model_out(df_ensembles,
   target_data_qntl,
   metrics = "wis"
 ) |>
   # calculate importance scores: subtract the error of the ensemble-all
   mutate(importance = wis - wis[1]) |>
   filter(model_id != "ens_all")
-ens_df$model_id <- sub("^ens_wo_", "", ens_df$model_id)
-# expected importance scores with quantile output and linear pool
-exp_imp_qntl1 <- ens_df |>
+
+# get dataframe including model_id and each model's importance score
+model_imp_scores <- ensemble_scores |>
   select(model_id, importance) |>
+  mutate(model_id = sub("^ens_wo_", "", model_id))
+
+# expected importance scores with quantile output and linear pool
+exp_imp_qntl1 <- model_imp_scores |>
   mutate(
     ens_mthd = "linear_pool-NA",
     algorithm = "lomo",
@@ -53,12 +51,12 @@ exp_imp_qntl1 <- ens_df |>
   as.data.frame()
 
 ## Case 2: no missing data and 'simple_ensemble' using agg_fun = mean
-# ensemble built from all models
-ens_df_simple <- simple_ensemble(
+# create an ensemble using all models and store it in 'df_ensembles_simple'
+df_ensembles_simple <- simple_ensemble(
   dat_qntl,
   model_id = "ens_all", agg_fun = "mean"
 )
-# construct ensembles without each model
+# create ensembles without each model and add them in the 'df_ensembles_simple'
 model_names <- unique(dat_qntl$model_id)
 for (i in seq_along(model_names)) {
   sub_data <- dat_qntl |> filter(model_id != model_names[i])
@@ -66,20 +64,24 @@ for (i in seq_along(model_names)) {
     model_id = paste0("ens_wo_", model_names[i]),
     agg_fun = "mean"
   )
-  ens_df_simple <- bind_rows(ens_df_simple, sub_ens)
+  df_ensembles_simple <- bind_rows(df_ensembles_simple, sub_ens)
 }
 # evaluate each ensemble
-ens_df_simple <- score_model_out(ens_df_simple,
+ensemble_scores <- score_model_out(df_ensembles_simple,
   target_data_qntl,
   metrics = "wis"
 ) |>
   # calculate importance scores: subtract the error of the ensemble-all
   mutate(importance = wis - wis[1]) |>
   filter(model_id != "ens_all")
-ens_df_simple$model_id <- sub("^ens_wo_", "", ens_df_simple$model_id)
-# expected importance scores with quantile output and simple mean ensemble
-exp_imp_qntl2 <- ens_df_simple |>
+
+# get dataframe including model_id and each model's importance score
+model_imp_scores <- ensemble_scores |>
   select(model_id, importance) |>
+  mutate(model_id = sub("^ens_wo_", "", model_id))
+
+# expected importance scores with quantile output and simple mean ensemble
+exp_imp_qntl2 <- model_imp_scores |>
   mutate(
     ens_mthd = "simple_ensemble-mean",
     algorithm = "lomo",
@@ -88,12 +90,12 @@ exp_imp_qntl2 <- ens_df_simple |>
   as.data.frame()
 
 ## Case 3: no missing data and 'simple_ensemble' using agg_fun = median
-# ensemble built from all models
-ens_df_simple <- simple_ensemble(
+# create an ensemble using all models and store it in 'df_ensembles_simple'
+df_ensembles_simple <- simple_ensemble(
   dat_qntl,
   model_id = "ens_all", agg_fun = "median"
 )
-# construct ensembles without each model
+# create ensembles without each model and add them in the 'df_ensembles_simple'
 model_names <- unique(dat_qntl$model_id)
 for (i in seq_along(model_names)) {
   sub_data <- dat_qntl |> filter(model_id != model_names[i])
@@ -101,20 +103,24 @@ for (i in seq_along(model_names)) {
     model_id = paste0("ens_wo_", model_names[i]),
     agg_fun = "median"
   )
-  ens_df_simple <- bind_rows(ens_df_simple, sub_ens)
+  df_ensembles_simple <- bind_rows(df_ensembles_simple, sub_ens)
 }
 # evaluate each ensemble
-ens_df_simple <- score_model_out(ens_df_simple,
+ensemble_scores <- score_model_out(df_ensembles_simple,
   target_data_qntl,
   metrics = "wis"
 ) |>
   # calculate importance scores: subtract the error of the ensemble-all
   mutate(importance = wis - wis[1]) |>
   filter(model_id != "ens_all")
-ens_df_simple$model_id <- sub("^ens_wo_", "", ens_df_simple$model_id)
-# expected importance scores with quantile output and simple median ensemble
-exp_imp_qntl3 <- ens_df_simple |>
+
+# get dataframe including model_id and each model's importance score
+model_imp_scores <- ensemble_scores |>
   select(model_id, importance) |>
+  mutate(model_id = sub("^ens_wo_", "", model_id))
+
+# expected importance scores with quantile output and simple median ensemble
+exp_imp_qntl3 <- model_imp_scores |>
   mutate(
     ens_mthd = "simple_ensemble-median",
     algorithm = "lomo",
@@ -125,27 +131,34 @@ exp_imp_qntl3 <- ens_df_simple |>
 ## Case 4: a missing data and 'simple_mean' ensemble
 # data with missing values
 sub_dat_qntl <- dat_qntl |> filter(model_id %in% model_id_list[c(1, 3)])
-# ensemble built from all models
-ens_df <- simple_ensemble(sub_dat_qntl, model_id = "ens_all")
-# construct ensembles without each model
+# create an ensemble using all models and store it in 'df_ensembles'
+df_ensembles <- simple_ensemble(sub_dat_qntl, model_id = "ens_all")
+# create ensembles without each model and add them in the 'df_ensembles'
 model_names <- unique(sub_dat_qntl$model_id)
 for (i in seq_along(model_names)) {
   sub_data <- sub_dat_qntl |> filter(model_id != model_names[i])
   sub_ens <- simple_ensemble(sub_data,
     model_id = paste0("ens_wo_", model_names[i])
   )
-  ens_df <- bind_rows(ens_df, sub_ens)
+  df_ensembles <- bind_rows(df_ensembles, sub_ens)
 }
 # evaluate each ensemble
-ens_df <- score_model_out(ens_df, target_data_qntl, metrics = "wis") |>
+ensemble_scores <- score_model_out(df_ensembles,
+  target_data_qntl,
+  metrics = "wis"
+) |>
   # calculate importance scores: subtract the error of the ensemble-all
   mutate(importance = wis - wis[1]) |>
   filter(model_id != "ens_all")
-ens_df$model_id <- sub("^ens_wo_", "", ens_df$model_id)
+
+# get dataframe including model_id and each model's importance score
+model_imp_scores <- ensemble_scores |>
+  select(model_id, importance) |>
+  mutate(model_id = sub("^ens_wo_", "", model_id))
+
 # expected importance scores with quantile output and simple mean ensemble
 exp_imp_qntl4 <- data.frame(model_id = model_id_list) |>
-  left_join(ens_df, by = "model_id") |>
-  select(model_id, importance) |>
+  left_join(model_imp_scores, by = "model_id") |>
   mutate(
     ens_mthd = "simple_mean",
     algorithm = "lomo",
@@ -159,12 +172,6 @@ exp_imp_qntl <- rbind(
 )
 
 # save data
-saveRDS(dat_qntl,
-  file = "tests/testthat/testdata/dat_qntl.rds"
-)
 saveRDS(exp_imp_qntl,
   file = "tests/testthat/testdata/exp_imp_qntl_untrained_lomo.rds"
-)
-saveRDS(target_data_qntl,
-  file = "tests/testthat/testdata/target_qntl.rds"
 )
