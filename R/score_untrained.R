02b44840feb6ec55dcb0db1f349161d6f7a6473c
Minsu Kim <minsu@umass.edu>
2025-06-09 21:03:48 -0400

    added lasomo algorithm in `score_untrained()` (#28)
    * relocate `ens_fun` object as it is needed for lasomo
    
    * change the format from `dataframe` to `tibble`.
    
    * Add `.lintr` to `.Rbuildignore` to suppress R CMD check NOTE
    
    * added importFrom("utils", "combn")
    
    * add a description for @param importance_algorithm regarding the `furrr` and `future` packages.
    
    * add 'ignore_attr' option to `expect_equal()`
    
    * Include code for the 'lasomo' algorithm
    
    * change the file name to indicate that it is for testing using the LOMO algorithm
    
    * file to generate expected importance scores for mean output forecasts using untrained ensembles in the LASOMO algorithm
    
    * use "lomo" for the algorithm variable as this is for testing only using LOMO
    
    * add dot-dot-dot to ens_fun, use quotes in `select()` instead of `.data$`, and use `if-else` statement
    
    * correct test purpose
    
    * dataset of the expected importance scores for the mean output type
    
    * unit tests using the LASOMO algorithm
    
    * Change the file name to include the output type.
    
    * dataset of the expected importance scores for the median output type
    
    * Include testing for the median output
    
    * change not to include `output_type_id` and `value` in the output
    
    * fix typo in comments
    
    * Include testing for the quantile output
    
    * dataset of the expected importance scores for the quantile output type
    
    * dataset of the expected importance scores for the pmf output type
    
    * Include testing for the pmf output
    
    * simplify a comment

--- R/score_untrained.R
+++ R/score_untrained.R
@@ -24,6 +24,7 @@
 #' @import hubEnsembles
 #' @import hubEvals
 #' @importFrom utils getFromNamespace
+#' @importFrom utils combn
 #' @inherit model_importance details
 
 score_untrained <- function(single_task_data, oracle_output_data, model_id_list,
@@ -32,16 +33,18 @@ score_untrained <- function(single_task_data, oracle_output_data, model_id_list,
   # models in the single_task_data
   models <- unique(single_task_data$model_id)
   missing_model <- setdiff(model_id_list, single_task_data$model_id)
+
+  # `ens_fun` is a function from hubEnsembles specified by `ensemble_fun`
+  ens_fun <- getFromNamespace(ensemble_fun, ns = asNamespace("hubEnsembles"))
+
   # Compute importance score when importance_algorithm is 'lomo'
   if (importance_algorithm == "lomo") {
-    # `ens_fun` is a function from hubEnsembles specified by `ensemble_fun`
-    ens_fun <- getFromNamespace(ensemble_fun, ns = asNamespace("hubEnsembles"))
+    # build an ensemble with the specified method using all models available
     ens_all <- ens_fun(single_task_data,
       weights = NULL,
       model_id = "ensemble-all",
       ...
     )
-
     # build ensemble forecasts by leaving one model out
     ens_lomo <- lapply(models, function(x) {
       single_task_data |>
@@ -52,8 +55,9 @@ score_untrained <- function(single_task_data, oracle_output_data, model_id_list,
           ...
         )
     })
+    # store all ensemble forecasts in a dataframe
     ensemble_data <- rbind(ens_all, dplyr::bind_rows(ens_lomo))
-    # score the ensemble forecasts
+    # score the ensemble forecasts and add it to the input dataset
     score_ens_all <- score_model_out(ensemble_data,
       oracle_output_data,
       metrics = metric
@@ -61,7 +65,8 @@ score_untrained <- function(single_task_data, oracle_output_data, model_id_list,
       rename(calculated_metric = any_of(
         c("ae_point", "se_point", "wis", "log_score")
       )) |>
-      left_join(ensemble_data, by = "model_id")
+      left_join(ensemble_data, by = "model_id") |>
+      as_tibble()
     # calculate importance scores
     ensemble_all_value <- score_ens_all |>
       dplyr::filter(.data$model_id == "ensemble-all") |>
@@ -72,18 +77,137 @@ score_untrained <- function(single_task_data, oracle_output_data, model_id_list,
       ) |>
       dplyr::filter(.data$model_id != "ensemble-all") |>
       dplyr::mutate(model_id = gsub("ens.wo.", "", .data$model_id)) |>
-      dplyr::select(-"calculated_metric")
+      dplyr::select(-"calculated_metric") |>
+      select(-c("output_type_id", "value")) |>
+      distinct()
   } else {
-    df_importance <- NULL
+    # check if the necessary packages are installed
+    if (!requireNamespace("furrr", quietly = TRUE)) {
+      stop("Please install `furrr` package for parallel execution in `lasomo`
+           algorithm.")
+    }
+    if (!requireNamespace("future", quietly = TRUE)) {
+      stop("Please install `future` package for parallel execution in `lasomo`
+           algorithm.")
+    }
+
+    # number of models
+    n <- length(models)
+    # Power set of {1,2,...,n} not including the empty set.
+    # We use this power set to get indices for subset of models
+    subsets <- lapply(1:n, function(x) combn(n, x, simplify = FALSE)) |>
+      unlist(recursive = FALSE)
+    # data frame of all possible ensemble forecasts
+    dat_all_ens <- purrr::map_dfr(
+      subsets,
+      function(subset) {
+        get_modelsubset <- models[subset]
+        # index of the subsets list that is identical to the current subset, S
+        i <- Position(function(x) identical(x, subset), subsets)
+        if (subset_wt == "perm_based") {
+          # calculate the weight given to this subset
+          weight <- 1 / ((n - 1) * choose(n - 1, length(get_modelsubset)))
+          # when the subset includes all indices, its weight is infinite
+          # replace it with NA (this value won't be used)
+          weight <- ifelse(is.infinite(weight), NA, weight)
+        } else {
+          # when subset_wt="equal"
+          weight <- 1
+        }
+        # reduced data including the models in the subset S
+        data_subset <- single_task_data |>
+          filter(.data$model_id %in% get_modelsubset)
+        # build an ensemble forecast using the models in the subset S
+        ensemble_forecast <- ens_fun(data_subset,
+          model_id = paste0("ensemble_", i), ...
+        )
+        # add index and weight to the ensemble forecast
+        ens_dat <- ensemble_forecast |>
+          mutate(subset_idx = i, subset_weight = weight)
+        ens_dat
+      }
+    )
+    # score the ensemble forecasts
+    score_ens_all <- score_model_out(
+      # remove subset info cols to avoid errors caused by the weight of NA
+      dat_all_ens |> select(-c("subset_idx", "subset_weight")),
+      oracle_output_data,
+      metrics = metric
+    ) |>
+      rename(calculated_metric = any_of(
+        c("ae_point", "se_point", "wis", "log_score")
+      )) |>
+      left_join(
+        dat_all_ens |>
+          select(c("model_id", "subset_weight")) |>
+          distinct(),
+        by = "model_id"
+      )
+
+    ## Environment for parallel computation
+    # store the original plan
+    original_plan <- future::plan()
+    # set parallel plan with a conservative number of workers
+    future::plan(future::multisession,
+      workers = min(4, parallel::detectCores() - 1)
+    )
+    # restore the original plan on exit
+    on.exit(future::plan(original_plan), add = TRUE)
+
+    ## parallel computation
+    # calculate importance scores
+    result <- furrr::future_map_dfr(1:n, function(j) {
+      # identify which in 'subsets' includes the value j, and return indices
+      set_incl_j <- which(sapply(subsets, function(x) j %in% x))
+      # find indices in 'subsets' list including more elements in addition to j
+      set_incl_j_more <- set_incl_j[set_incl_j > n]
+
+      # weighted marginal contribution of the jth model to each subset
+      scores_by_subset <- purrr::map_dbl(
+        set_incl_j_more,
+        function(k) {
+          # get elements of the subset that includes j
+          set_k <- subsets[[k]]
+          # index in 'subsets' list that include elements of set_k except for j
+          k1 <- which(sapply(subsets, setequal, set_k[set_k != j]))
+          # calculated_metric for the ensemble forecast including jth model
+          score_incl_j <- score_ens_all$calculated_metric[k]
+          # calculated_metric for the ensemble forecast not including jth model
+          score_not_incl_j <- score_ens_all$calculated_metric[k1]
+          # get the subset weight for the current subset
+          subset_weight <- score_ens_all$subset_weight[k1]
+          # jth model's marginal contribution multiplied by the subset weight
+          subset_weight * (-score_incl_j + score_not_incl_j)
+        }
+      )
+      # accumulate the scores calculated by subsets
+      score <- sum(scores_by_subset)
+      # store the score for the jth model depending on the subset_wt option
+      if (subset_wt == "perm_based") {
+        data.frame(
+          model_id = models[j],
+          importance = score
+        )
+      } else {
+        data.frame(
+          model_id = models[j],
+          importance = score / (2^(n - 1) - 1)
+        )
+      }
+    })
+    # add the scores column to the input dataset
+    df_importance <- single_task_data |>
+      select(-c("output_type_id", "value")) |>
+      distinct() |>
+      left_join(result, by = "model_id")
   }
   # Insert NAs for missing models
   if (length(missing_model) > 0) {
     fixed_cols <- df_importance |>
-      select(-c("model_id", "value", "importance")) |>
+      select(-c("model_id", "importance")) |>
       distinct()
     missing_model_rows <- data.frame(
-      model_id = missing_model, fixed_cols,
-      value = NA, importance = NA
+      model_id = missing_model, fixed_cols, importance = NA
     )
     # bind the new rows to df_importance
     combined_df <- bind_rows(df_importance, missing_model_rows)
@@ -93,11 +217,9 @@ score_untrained <- function(single_task_data, oracle_output_data, model_id_list,
         combined_df,
         by = "model_id"
       ) |>
-      select(-c("output_type_id", "value")) |>
       distinct()
   } else {
     importance_scores <- df_importance |>
-      select(-c("output_type_id", "value")) |>
       distinct()
   }
   importance_scores
