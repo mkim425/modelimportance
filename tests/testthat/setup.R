b42d4ac740386b9eb1154f242ae79971b11acb2a
mkim425 <minsu@umass.edu>
2025-07-04 10:28:35 -0400

    explicitly load the `future` package

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,5 +1,7 @@
-if (Sys.getenv("CI") == "true" && Sys.info()["sysname"] == "Darwin") {
-        future::plan(sequential)
-} else {
-        future::plan(multisession)
+if (requireNamespace("future", quietly = TRUE)) {
+  if (Sys.getenv("CI") == "true" && Sys.info()["sysname"] == "Darwin") {
+    future::plan(future::sequential)
+  } else {
+    future::plan(future::multisession)
+  }
 }
