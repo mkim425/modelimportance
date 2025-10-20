80e13e43d4c82c14eda3062a3a06b781c063edc4
mkim425 <minsu@umass.edu>
2025-10-20 13:43:23 -0400

    change an argument name to avoid confusion

--- R/adjust_metric.R
+++ R/adjust_metric.R
@@ -4,21 +4,21 @@
 #' for `log_score` metric, replace -Inf values with min_log_score.
 #'
 #' @param df A data.frame containing metric values.
-#' @param min_log_score A numeric value to replace -Inf log_score values.
+#' @param log_min_val A numeric value to replace -Inf log_score values.
 #' Default is -10.
 #'
 #' @returns A data.frame with adjusted metric values.
 #' @noRd
-adjust_metric <- function(df, min_log_score) {
+adjust_metric <- function(df, log_min_val = -10) {
   # if metric is se_point, convert it to rse_point
   if ("se_point" %in% names(df)) {
     df <- df |>
       mutate(rse_point = sqrt(.data$se_point)) |>
       select(-"se_point")
   }
-  # if metric is log_score and the value is Inf, convert it to min_log_score
+  # if metric is log_score and the value is Inf, convert it to log_min_val
   if ("log_score" %in% names(df) && any(is.infinite(df$log_score))) {
-    if (min_log_score == -10) {
+    if (log_min_val == -10) {
       message(paste(
         "A negative infinite log_score value ('-Inf') occurred",
         "due to the zero probability assigned to the true outcome. \n",
@@ -29,11 +29,11 @@ adjust_metric <- function(df, min_log_score) {
     } else {
       message(paste0(
         "A negative infinite log_score value ('-Inf') is replaced with ",
-        min_log_score, "."
+        log_min_val, "."
       ))
     }
 
-    df$log_score[df$log_score == Inf] <- min_log_score
+    df$log_score[df$log_score == -Inf] <- log_min_val
   }
   df
 }
