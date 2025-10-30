874e527dd8de08568bb39c04550c7c2107b3c2ae
mkim425 <minsu@umass.edu>
2025-10-30 10:24:22 -0400

    Validate `min_log_score` and the combination of weighted and training_window_length;

--- R/validate_inputs.R
+++ R/validate_inputs.R
@@ -9,7 +9,8 @@ validate_inputs <- function(forecast_data, oracle_output_data,
                             training_window_length = 0,
                             importance_algorithm = c("lomo", "lasomo"),
                             subset_wt = c("equal", "perm_based"),
-                            na_action = c("worst", "average", "drop")) {
+                            na_action = c("worst", "average", "drop"),
+                            min_log_score = -10) {
   window_len <- training_window_length
   # validate inputs
   if (!is.data.frame(forecast_data)) {
@@ -25,6 +26,21 @@ validate_inputs <- function(forecast_data, oracle_output_data,
   if (!(is.numeric(window_len) && window_len == as.integer(window_len))) {
     stop("Invalid value for 'training_window_length'. It must be an integer.
          Default is 0.")
+  }
+  if (weighted && window_len == 0) {
+    stop(paste0(
+      "Invalid combination: 'training_window_length' should be ",
+      "a positive integer when 'weighted' is TRUE."
+    ))
+  }
+  if (!weighted && window_len != 0) {
+    message(
+      "Note: 'training_window_length' is ignored when 'weighted' is FALSE."
+    )
+  }
+  if (min_log_score >= 0) {
+    stop("Invalid value for 'min_log_score'. It must be a negative number.
+         Default is -10.")
   }
   ensemble_fun <- match.arg(ensemble_fun)
   importance_algorithm <- match.arg(importance_algorithm)
