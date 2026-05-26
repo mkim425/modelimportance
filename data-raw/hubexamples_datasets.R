c7834d8a7be5bc418b72cee1d95b65ad7c5c05e7
mkim425 <minsu@umass.edu>
2026-05-26 16:17:54 -0400

    subset the data files from the `hubExamples` package to reduce package size

--- data-raw/hubexamples_datasets.R
+++ data-raw/hubexamples_datasets.R
@@ -2,10 +2,41 @@
 # Run this script to regenerate data/ if hubExamples is updated.
 
 library(hubExamples)
+library(dplyr)
 
-forecast_outputs <- hubExamples::forecast_outputs
-forecast_target_ts <- hubExamples::forecast_target_ts
-forecast_oracle_output <- hubExamples::forecast_oracle_output
+# Dates used across all vignettes and examples
+example_dates <- as.Date(c(
+  "2022-11-19",
+  "2022-11-26",
+  "2022-12-03",
+  "2022-12-10",
+  "2022-12-17",
+  "2022-12-24",
+  "2022-12-31",
+  "2023-01-07"
+))
+
+# median (both locations) + quantile (location "25" only)
+forecast_outputs <- hubExamples::forecast_outputs |>
+  filter(
+    (output_type == "median" & location %in% c("25", "48")) |
+      (output_type == "quantile" & location == "25")
+  )
+
+# observed values for the two locations and dates used in vignettes
+forecast_target_ts <- hubExamples::forecast_target_ts |>
+  filter(
+    location %in% c("25", "48"),
+    target_end_date %in% example_dates,
+    target == "wk inc flu hosp"
+  )
+
+# oracle output for the two dates and locations used in get-testdata script
+forecast_oracle_output <- hubExamples::forecast_oracle_output |>
+  filter(
+    target_end_date %in% as.Date(c("2022-11-26", "2022-12-10")),
+    location %in% c("25", "48")
+  )
 
 usethis::use_data(
   forecast_outputs,
